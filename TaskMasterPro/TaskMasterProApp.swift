9231a3cef6386e3bd104809a85943329cbd77bde
joshuacalebs <20627418+joshuacalebs@users.noreply.github.com>
2024-10-09 13:14:18 -0500

    Initial Commit

---
+++ TaskMasterPro/TaskMasterProApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskMasterProApp.swift
+//  TaskMasterPro
+//
+//  Created by Joshua Shirreffs on 10/9/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskMasterProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
