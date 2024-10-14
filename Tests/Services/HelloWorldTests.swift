7190167580482ae7d036e481b7389fac43c72f9d
taskmanaged <joshua.shirreffs@taskmanagedinc.com>
2024-10-14 11:20:04 -0500

    Delete Tests/Services directory

--- Tests/Services/HelloWorldTests.swift
+++
@@ -1,9 +0,0 @@
-import XCTest
-@testable import [project-name]
-
-class HelloWorldTests: XCTestCase {
-    func testHelloWorld() {
-        let helloWorld = HelloWorld()
-        XCTAssertEqual(helloWorld.sayHello(), "Hello, World!")
-    }
-}
